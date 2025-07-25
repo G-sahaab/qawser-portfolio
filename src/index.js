d52d902094c0d6a146fde0068609f5c0f718f524
Qawser Qayoom <142694538+G-sahaab@users.noreply.github.com>
2025-07-25 12:46:48 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import QawserPortfolio from "./QawserPortfolio";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <QawserPortfolio />
+  </React.StrictMode>
+);
